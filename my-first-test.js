aec976c86a800ec5f917bb1ab6035131064689e8
Thanura <thanudilan@ymail.com>
2025-06-15 16:28:58 +0530

    Changes were commited on 15/06/25

--- my-first-test.js
+++ my-first-test.js
@@ -3,7 +3,13 @@ import {sleep} from 'k6';
 
 export const options = {
     vus: 10,
-    duration: '30s',
+    stages: [
+        { duration: '30s', target: 20 },
+        { duration: '1m30s', target: 10 },
+        { duration: '15s', target: 5 },
+        { duration: '30s', target: 15 },
+        { duration: '20s', target: 0 },
+    ]
 }
 
 export default () => {
