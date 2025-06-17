3ad3f631eec7207d04bdd2ec9b988d2362511c57
Thanura <thanudilan@ymail.com>
2025-06-17 18:00:56 +0530

    Committed the changes on 17/06/25

--- Tests/my-third-test.ts
+++ Tests/my-third-test.ts
@@ -11,6 +11,12 @@ export const options: Options = {
     { duration: "10s", target: 100 },
     { duration: "10s", target: 0 },
   ],
+
+  thresholds: {
+    http_req_duration: ["p(95)<500"],
+    http_req_failed: ["rate<0.01"],
+    checks: ["rate>0.99"],
+  },
 };
 
 export default () => {
